d17bb20de33825383db4593b1824859116e4b9f3
Jessica Lima <jsdl.9991@gmail.com>
2022-06-27 19:56:18 -0300

    criando projeto spring boot

---
+++ src/main/java/com/serasa/erestrito/ErestritoApplication.java
@@ -0,0 +1,13 @@
+package com.serasa.erestrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErestritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErestritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serasa/erestrito/ErestritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serasa.erestrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErestritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
