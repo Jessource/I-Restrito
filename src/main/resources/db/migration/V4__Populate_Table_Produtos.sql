4c40183fa883e7b84b93563ab5ca48c6da22c59b
Jessica Lima <jsdl.9991@gmail.com>
2022-07-22 15:44:57 -0300

    removendo migrations com problemas

--- src/main/resources/db/migration/V4__Populate_Table_Produtos.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `produtos` (`nome`, `descricao`, `marca`, `origem`, `adicao`, `tipo_produto`, `restricao`) VALUES 
-('Administrador', '', 'admin@admin.com', '$2a$10$gOGM9oD3iOXkdZAZ1DGtXe4fRYJe4p0Io0137XyPCE7O5vzRpMeve', 'SP', '1988-02-18', 'ADMIN')
